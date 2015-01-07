712ef0f8b7a4fa7be16ee0979b03bdf6369fc12f
bontaq <bontaq@gmail.com>
2015-01-07 16:47:26 -0500

    last elm commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,14 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        elm: {
-            compile: {
-                files: {
-                    'dogebote/static/js/main.js': 'elm/Main.elm'
-                }
-            }
-        },
         watch: {
             scripts: {
-                files: ['elm/Main.elm'],
-                tasks: ['elm']
+                files: [],
+                tasks: []
             }
         }
     });
-    grunt.loadNpmTasks('grunt-elm');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass')
 
-    grunt.registerTask('default', ['elm']);
+    grunt.registerTask('default', []);
 };
