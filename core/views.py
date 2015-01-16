e94751f9cd5c180cf0daba7e2bbee06fefd5f8d6
bontaq <bontaq@gmail.com>
2015-01-15 19:47:31 -0500

    Include a display of the last 50 lines from logfile

--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
-import soundcloud
+from subprocess import call, check_output
+import json
+import os
+from django.conf import settings
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response, redirect
 
 
-# Create your views here.
 def home(request):
     return render_to_response('index.html', RequestContext(request, {
 
     }))
+
+
+def get_log(request):
+    """Returns the last 50 lines of the main log file"""
+
+    output = check_output(['tail', '-n', '50', 'debug.log'])
+    results = str(output).split('\n')
+    return HttpResponse(json.dumps({'objects': results}), content_type='application/json')
